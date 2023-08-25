eead2b9933e62d8c266e235b9ef11efd48aa0ef4
Sue Ann Koay <koays@janelia.hhmi.org>
2023-08-25 17:33:22 -0400

    Renamed training script.

---
+++ scripts/train_keypoints.py
@@ -0,0 +1,43 @@
+def main(data_spec):
+    import os
+    from ultralytics import YOLO
+    from ultralytics import settings
+
+    settings.update({
+        "runs_dir": os.path.join(os.path.dirname(data_spec), "runs"),
+        "tensorboard": True,
+    })
+
+    # Create a new YOLO model from scratch
+    # model = YOLO('yolov8n.yaml')
+
+    # Load a pretrained YOLO model (recommended for training)
+    model = YOLO('yolov8n-pose.pt')
+
+    # Train the model using the 'coco128.yaml' dataset for 3 epochs
+    results = model.train(data=data_spec,
+                          epochs=1000, imgsz=640, batch=64,
+                          save=True, save_period=100,
+                          verbose=True,
+                          cache=True, workers=2,
+                          device=0,
+                          augment=True,
+                          flipud=0.3,  # image flip up-down (probability)
+                          )
+    print(results)
+
+    # Evaluate the model's performance on the validation set
+    results = model.val(device=1)
+    print(results)
+
+    # Perform object detection on an image using the model
+    # results = model("D:/Data/081823_masks_on/images/val/081823_masks_on_20230818-1353_cam21492306_frame600.png")
+    # print(results)
+
+    # Export the model to ONNX format
+    # success = model.export(format='onnx')
+
+
+if __name__ == "__main__":
+    # freeze_support()
+    main("D:/Data/081823_masks_on_spine/081823_masks_on_spine.yaml")
